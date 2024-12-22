1ba3750f6d8718b137baa700ef0fbc6fb1f4f799
Ambre Viau <ambre.viau@universite-paris-saclay.fr>
2024-12-22 20:46:33 +0100

    api

---
+++ web/controller/apiTest.php
@@ -0,0 +1,109 @@
+<?php
+require_once ('../config/connexion.php'); 
+require_once ('../modele/modele.php');
+
+
+
+
+    // Mettre en attribut ce qu'il y a en dessous
+header("Content-Type:application/json"); // le format du corps de la requete est JSON
+$pdo = Connexion::getConnection();
+$method = $_SERVER['REQUEST_METHOD'];
+$path = explode('/', trim($_SERVER['PATH_INFO'] ?? $_SERVER['REQUEST_URI'], '/'));
+
+if (!is_null($path[0])){
+	$table=$path[0];
+    try {
+        if ($method == 'GET') {
+			$requete = "select * from $table";
+			$i=0;
+			$j=0;
+			//format : $table/$connection/$table2
+			while(isset($path[$i+2])){
+				$j=$i+1;
+				$k=$i;
+				$i=$i+2;
+				$requete = $requete . " inner join $path[$i] on $path[$i].$path[$j] = $path[$k].$path[$j]";
+				
+			}
+			$requete = $requete . ";";
+            $stmt = $pdo->query($requete);
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            echo json_encode($result);   
+        } elseif($method == 'POST') {
+            $data = json_decode(file_get_contents('php://input'), true);
+            $stmt = $pdo->prepare('insert into utilisateur (nom_utilisateur, prenom_utilisateur, adr_utilisateur, cp_utilisateur, mail_utilisateur, login_utilisateur, pdp_utilisateur 
+                                values (:nom, :prenom, :adr, :cp, :mail, :login, :pdp) ');
+            $stmt->execute([
+                ':id' => $data['id_utilisateur'],
+                ':nom' => $data['nom_utilisateur'],
+                ':prenom' => $data['prenom_utilisateur'],
+                ':adr' => $data['adr_utilisateur'],
+                ':cp' => $data['cp_utilisateur'],
+                ':mail' => $data['mail_utilisateur'],
+                ':login' => $data['login_utilisateur'],
+                ':pdp' => $data['pdp_utilisateur'],
+            ]);
+            http_response_code(201); // It means the request succeeded, a new resource was created as a result 
+            echo json_encode(['message' => 'Utilisateur ajouté avec succès']);
+            //break;
+            
+        } elseif ($method == 'PUT') {
+            if(isset($path[1])) {
+                $id = intval($path[1]);
+                $data = json_decode(file_get_contents('php://input'), true);
+                $stmt = $pdo->prepare('update utilisateurs set nom_utilisateur = :nom, prenom_utilisateur = :prenom, adr_utilisateur = :adr, cp_utilisateur = :cp, 
+                                        mail_utilisateur = :mail, login_utilisateur = :login, pdp_utilisateur = :pdp
+                                        where id_utilisateur = :id');
+                $stmt->execute([
+                    ':nom' => $data['nom_utilisateur'],
+                    ':prenom' => $data['prenom_utilisateur'],
+                    ':adr' => $data['adr_utilisateur'],
+                    ':cp' => $data['cp_utilisateur'],
+                    ':mail' => $data['mail_utilisateur'],
+                    ':login' => $data['login_utilisateur'],
+                    ':pdp' => $data['pdp_utilisateur'],
+                    ':id' => $id,
+                ]);
+                http_response_code(201); // Code status : POST/PUT succeeded
+                echo json_encode(['message' => 'Utilisateur mis à jour avec succès']);
+            } else {
+                http_response_code(404);
+                echo json_encode(['message' => 'ID user demandé']);
+            }
+            //break;
+    
+    
+        } elseif ($method == 'DELETE') {
+            if(isset($path[1])) {
+                $id = intval($path[1]);
+                $data = json_decode(file_get_contents('php://input'), true);
+                $stmt = $pdo->prepare('delete from utilisateurs where id_utilisateur = :id');
+                $stmt->execute(['id' => $id]);
+                http_response_code(201);
+                echo json_encode(['message' => 'Utilisateur supprime']);
+            } else {
+                http_response_code(400); // Bad request : ID User false
+                echo json_encode(['message' => 'ID User demandé']);
+            } 
+            //break;
+
+        } else {
+            http_response_code(405);
+            echo json_encode(['message' => 'Méthode pas autorise']);
+        }
+        
+        
+        } catch (Exception $e) {
+            http_response_code(500); //Method not allowed
+            echo json_encode(['message' => 'Méthode non autorise']);
+            echo $e->getMessage();
+
+        }
+} else {
+    http_response_code(404);
+    echo json_encode(['message' => 'Endpoint non trouve']);
+}
+
+
+?>
--- web/vue/acceuil.php
+++ web/vue/acceuil.php
@@ -11,16 +11,20 @@
 	
 	<h1> Mes groupes </h1>
 	<?php 
-		$curl = curl_init('https://projets.iut-orsay.fr/saes3-aviau/TestProket/Web/controller/api.php/utilisateur/?method=GET');
+		//$curl = curl_init('https://projets.iut-orsay.fr/saes3-aviau/TestProket/Web/controller/api.php/utilisateur/?method=GET');
+		$curl = curl_init('https://projets.iut-orsay.fr/saes3-aviau/TestProket/Web/controller/apiTest.php/utilisateur/id_utilisateur/membre/id_groupe/groupe/id_Groupe/Thème/?method=GET');
 		curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
 		$data = json_decode(curl_exec($curl),true);
 		foreach ($data as $value){
-			echo "<div class='grp'style='background-color:Teal'>";
+			if($value['Mail_Utilisateur']==$_SESSION['user_id'] && $value['Banni']==0){
+				$color = $value['Couleur_Groupe'];
+				echo "<div class='grp'style='background-color:$color'>";
 				echo "<image class ='left' src='./images/iconImage.png' alt ='icon image'/>";
-				echo "<h2>" . $value['Login_Utilisateur'] . "</h2>";
+				echo "<h2>" . $value['Nom_Groupe'] . "</h2>";
+				
 				echo "<p> themes <p>";
 			echo "</div>";
-			
+			}
 		}
 		//print_r($data);
 	?>
