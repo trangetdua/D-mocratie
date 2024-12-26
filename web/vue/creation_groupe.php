dbaed7daca861de000901d5e0ee5b6b4ccda7fa8
Ambre Viau <ambre.viau@universite-paris-saclay.fr>
2024-12-26 16:20:12 +0100

    .

--- web/vue/creation_groupe.php
+++ web/vue/creation_groupe.php
@@ -3,42 +3,29 @@ require_once ('../config/connexion.php');
 
 try{
 $message ='';
-    $_POST['nom'];
-    $_POST['cocouleur'];
-	if(isset($theme1
-    echo $message;
+    $nom = $_POST['nom'];
+    $couleur = $_POST['couleur'];
+	if(isset($_POST['Theme1'])){
+		$theme1 = $_POST['Theme1'];
+	}
+	else{
+		$theme1 = null;
+	}
+	if(isset($_POST['Theme2'])){
+		$theme1 = $_POST['Theme2'];
+	}
+	else{
+		$theme2 = null;
+	}
 	$url ="https://projets.iut-orsay.fr/saes3-aviau/TestProket/Web/controller/api.php/utilisateur/$username/?method=GET";
 	echo $url;
     $curl = curl_init($url);
 	curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
-	$user = json_decode(curl_exec($curl),true);
-    echo "$username <br>";
-    if (is_null($user)){
-        echo "je vais me defenestrer";
-    }
-    echo "ldsch";
-    print_r($user);
-    echo "hdsijhcds";
+	curl_exec($curl);
+    $_SESSION['groupe']
+	$_SESSION['role'] = "administrateur";
+    header('Location: acceuil.php');
 
-    if ($user["message"] == "Utilisateur non trouvé"){
-        header('Location: conne.php?identifiant=faux');
-    }
-    
-    
-	elseif ($user && $password==$user[0]['Login_Utilisateur'] ) {
-        session_start();
-        $_SESSION['user_id'] =$user[0]['Mail_Utilisateur'];
-        header('Location: acceuil.php');
-        $message = "ça fonctionne";
-        echo $message;
-    } else {
-        header('Location: conne.php?identifiant=faux');
-
-    }
-
-
-
-}
 } catch (Exception $e) {
     echo $e->getMessage();
 }
