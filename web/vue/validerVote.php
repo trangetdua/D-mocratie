a4e24450e19825a18af852b161f4b22b340af7c2
Viau Ambre <ambre.viau@universite-paris-saclay.fr>
2025-01-07 23:03:34 +0100

    Replace validerVote.php

--- web/vue/validerVote.php
+++ web/vue/validerVote.php
@@ -3,19 +3,8 @@ require_once("isConnected.php");
 
 try{
 	$id = $_GET['id'];
-	$page = $_GET['page'];
-	$type = $_GET['type'];
 
-	$data = $curlUti = curl_init('https://projets.iut-orsay.fr/saes3-aviau/TestProket/Web/controller/api.php/Vote?method=GET');
-	curl_setopt($curlUti,CURLOPT_RETURNTRANSFER,1);
-	$utis = json_decode(curl_exec($curlUti),true);
-	for each($utis as $vote){
-		if($vote['Id_Vote']==$id){
-		$nombre = $vote['Signaler']
-		}
-	}
-	$nombre = $nombre +1;
-	$url="https://projets.iut-orsay.fr/saes3-aviau/TestProket/Web/controller/api.php/Vote/Id_Vote/9/Signaler/$nombre/?method=PUT ";
+	$url="https://projets.iut-orsay.fr/saes3-aviau/TestProket/Web/controller/api.php/Vote/Id_Vote/$id/valide/1/?method=PUT ";
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -23,7 +12,7 @@ try{
 
 
 		
-	header('Location: proposition.php');
+	header('Location: vote.php');
 }
 catch (Exception $e) {
     echo $e->getMessage();
