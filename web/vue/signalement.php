f0409a0c78d5649bfa088794e2143a0a2b11fb55
Viau Ambre <ambre.viau@universite-paris-saclay.fr>
2025-01-07 23:12:14 +0100

    Replace signalement.php

--- web/vue/signalement.php
+++ web/vue/signalement.php
@@ -2,14 +2,26 @@
 require_once("isConnected.php");
 
 try{
-	$aSignaler = $_GET['id'];
+
+	$id = $_GET['id'];
 	$page = $_GET['page'];
-	$type = $_GET['type'];
+	$table = $_GET['table'];
+	$nom = $_GET['nom'];
 
-	$data = (la tu fais un get) 
-	$nombre = $data['Signaler']
-	url de modication( je crois que cest PUT) ou tu modifie la collone signaler et tu la remplace par $nombre
-
+	$data = $curlUti = curl_init("https://projets.iut-orsay.fr/saes3-aviau/TestProket/Web/controller/api.php/$table?method=GET");
+	curl_setopt($curlUti,CURLOPT_RETURNTRANSFER,1);
+	$utis = json_decode(curl_exec($curlUti),true);
+	for each($utis as $info){
+		if($info[$nom]==$id){
+		$nombre = $info['Signaler']
+		}
+	}
+	$nombre = $nombre +1;
+	$url="https://projets.iut-orsay.fr/saes3-aviau/TestProket/Web/controller/api.php/$table/$nom/$id/Signaler/$nombre/?method=PUT ";
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($curl);
 
 		
 	header('Location: '.$page.'.php ');
