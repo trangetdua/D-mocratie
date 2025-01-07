34a385730d0a8c58b3fe76e06434d056296eb59b
Viau Ambre <ambre.viau@universite-paris-saclay.fr>
2025-01-07 22:58:14 +0100

    Replace validerVote.php

--- web/vue/validerVote.php
+++ web/vue/validerVote.php
@@ -2,13 +2,24 @@
 require_once("isConnected.php");
 
 try{
-	$ = $_GET['id'];
+	$id = $_GET['id'];
 	$page = $_GET['page'];
 	$type = $_GET['type'];
 
-	$data = (la tu fais un get) 
-	$nombre = $data['Signaler']
-	https://projets.iut-orsay.fr/saes3-aviau/TestProket/Web/controller/api.php/vote/id_vote/9/nom_groupe/Lama /?method=PUT $nombre
+	$data = $curlUti = curl_init('https://projets.iut-orsay.fr/saes3-aviau/TestProket/Web/controller/api.php/Vote?method=GET');
+	curl_setopt($curlUti,CURLOPT_RETURNTRANSFER,1);
+	$utis = json_decode(curl_exec($curlUti),true);
+	for each($utis as $vote){
+		if($vote['Id_Vote']==$id){
+		$nombre = $vote['Signaler']
+		}
+	}
+	$nombre = $nombre +1;
+	$url="https://projets.iut-orsay.fr/saes3-aviau/TestProket/Web/controller/api.php/Vote/Id_Vote/9/Signaler/$nombre/?method=PUT ";
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($curl);
 
 
 		
